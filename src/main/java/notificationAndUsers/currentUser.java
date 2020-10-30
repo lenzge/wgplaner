72b337563f7fa4bf52ac7740f3a24e89e6162d2f
Lea Gutierrez Lange <lg067@hdm-stuttgart.de>
2020-10-30 10:08:59 +0100

    30.10.: kleiner Anfang der Klassen

---
+++ src/main/java/notificationAndUsers/currentUser.java
@@ -0,0 +1,17 @@
+package notificationAndUsers;
+
+public class currentUser {
+
+    public void addNoteToBlackboard(){}
+    public void deleteNoteFromBlackboard(){}
+    public void pinNoteToBlackboard(){}
+    public void makeCalenderEntry(){}
+    public void editCalender(){}
+    public void makeTask(String content){}
+    public void finishTask(){}
+    public void editTask(){}
+    public void addItemToGroceryList(String item){
+
+    }
+    public void boughtItem(){}
+}
