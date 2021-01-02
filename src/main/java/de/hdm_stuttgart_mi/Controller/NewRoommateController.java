e09637d594efe42f1ac333850abf5d1bd583036d
lea <lg067@hdm-stuttgart.de>
2021-01-02 16:17:21 +0100

    scenechange at newRoommate

--- src/main/java/de/hdm_stuttgart_mi/Controller/NewRoommateController.java
+++ src/main/java/de/hdm_stuttgart_mi/Controller/NewRoommateController.java
@@ -4,11 +4,14 @@ import de.hdm_stuttgart_mi.notificationAndUsers.Navigation;
 import de.hdm_stuttgart_mi.notificationAndUsers.Roommate;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,58 +27,71 @@ import java.util.ResourceBundle;
 
 public class NewRoommateController extends Supercontroller implements Initializable {
     private static final Logger log = LogManager.getLogger(NewRoommateController.class);
-    private DateTimeFormatter formatter= DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     private LocalDate birthday;
     private String profilePic;
-    @FXML private DatePicker birthday_dp;
-    @FXML private HBox profilepicList;
-    @FXML private TextField firstname_tf;
-    @FXML private TextField lastname_tf;
-    @FXML private TextField phonenumber_tf;
-    @FXML private Label birthday_lb;
+    @FXML
+    private DatePicker birthday_dp;
+    @FXML
+    private HBox profilepicList;
+    @FXML
+    private TextField firstname_tf;
+    @FXML
+    private TextField lastname_tf;
+    @FXML
+    private TextField phonenumber_tf;
+    @FXML
+    private Label birthday_lb;
+    @FXML
+    private GridPane root;
     Navigation nav = new Navigation();
-    @FXML private void datepicked(){
+
+    @FXML
+    private void datepicked() {
         String value = birthday_dp.getValue().format(formatter);
-        birthday = LocalDate.parse(value,formatter);
+        birthday = LocalDate.parse(value, formatter);
         log.debug(birthday);
     }
-    @FXML private void apply(ActionEvent e) throws IOException {
-        boolean applyable=true;
-        String firstname="",lastname="",phonenumber="";
-        if(firstname_tf.getText() ==null || firstname_tf.getText().equals("")||firstname_tf.getText().matches("[^a-zA-Z]")){
+
+    @FXML
+    private void apply(ActionEvent e) throws IOException {
+        boolean applyable = true;
+        String firstname = "", lastname = "", phonenumber = "";
+        if (firstname_tf.getText() == null || firstname_tf.getText().equals("") || firstname_tf.getText().matches("[^a-zA-Z]")) {
             firstname_tf.setText("Bitte trage deinen Vornamen ein. Es sind keine Zahlen erlaubt");
             firstname_tf.setStyle("-fx-border-color: red");
-            applyable=false;}
-        else {firstname = firstname_tf.getText();}
-        if(lastname_tf.getText() ==null || lastname_tf.getText().equals("")||lastname_tf.getText().matches("[^a-zA-Z]")){
+            applyable = false;
+        } else {
+            firstname = firstname_tf.getText();
+        }
+        if (lastname_tf.getText() == null || lastname_tf.getText().equals("") || lastname_tf.getText().matches("[^a-zA-Z]")) {
             lastname_tf.setText("Bitte trage deinen Nachnamen ein. Es sind keine Zahlen erlaubt");
             lastname_tf.setStyle("-fx-border-color: red");
-            applyable=false;}
-        else{lastname = lastname_tf.getText();}
-        if(phonenumber_tf.getText().matches("^(\\d+)$"))
-        {
-         phonenumber = phonenumber_tf.getText();
+            applyable = false;
+        } else {
+            lastname = lastname_tf.getText();
         }
-        else {
+        if (phonenumber_tf.getText().matches("^(\\d+)$")) {
+            phonenumber = phonenumber_tf.getText();
+        } else {
             phonenumber_tf.setText("Nur Zahlen in der Telefonnummer erlaubt");
             phonenumber_tf.setStyle("-fx-border-color: red");
-            applyable=false;}
-        if(birthday_dp.getValue()==null){
-            birthday_dp.setStyle("-fx-border-color: red;");
-            applyable=false;
+            applyable = false;
         }
-        if(applyable)
-        {
-            Roommate newRoommate = new Roommate(firstname,lastname,0,phonenumber,true,LocalDate.now(),birthday,profilePic);
+        if (birthday_dp.getValue() == null) {
+            birthday_dp.setStyle("-fx-border-color: red;");
+            applyable = false;
+        }
+        if (applyable) {
+            Roommate newRoommate = new Roommate(firstname, lastname, 0, phonenumber, true, LocalDate.now(), birthday, profilePic);
             log.debug("Roommate created");
             nav.addCurrentRoomate(newRoommate);
-            int id= nav.getRoommate(nav.roommateListLenght()-1).getID();
+            int id = nav.getRoommate(nav.roommateListLenght() - 1).getID();
             nav = new Navigation(id);
 
             super.changeScene(e);
         }
-
 
 
     }
@@ -85,19 +101,19 @@ public class NewRoommateController extends Supercontroller implements Initializa
         ToggleGroup toggleGroup = new ToggleGroup();
 
         File directory = new File("src/main/resources/images");
-       //all Filenames in the directory are written into a Stringarray
+        //all Filenames in the directory are written into a Stringarray
         String[] list = directory.list();
-       //sicherstellen dass die Liste nicht null ist
+        //sicherstellen dass die Liste nicht null ist
         assert list != null;
-        for(String profilepicPath : list){
+        for (String profilepicPath : list) {
             ToggleButton button = new ToggleButton();
-            FileInputStream input = new FileInputStream("src/main/resources/images/"+profilepicPath);
-            Image profilePic= new Image(input);
+            FileInputStream input = new FileInputStream("src/main/resources/images/" + profilepicPath);
+            Image profilePic = new Image(input);
             ImageView showProfilePic = new ImageView(profilePic);
             showProfilePic.setFitWidth(100);
             showProfilePic.setFitHeight(100);
             button.setGraphic(showProfilePic);
-            button.setId("src/main/resources/images/"+profilepicPath);
+            button.setId("src/main/resources/images/" + profilepicPath);
 
             button.setOnAction(event -> profilButton(button));
 
@@ -105,27 +121,44 @@ public class NewRoommateController extends Supercontroller implements Initializa
             profilepicList.getChildren().add(button);
         }
     }
-   private void profilButton(ToggleButton button){
+
+    private void profilButton(ToggleButton button) {
 
         profilePic = button.getId();
         button.setSelected(true);
         log.debug(profilePic);
-   }
+    }
 
-   @FXML private void clear(MouseEvent mouseEvent){
-        if(mouseEvent.getSource() instanceof TextField){
+    @FXML
+    private void clear(MouseEvent mouseEvent) {
+        if (mouseEvent.getSource() instanceof TextField) {
             TextField textfield = (TextField) mouseEvent.getSource();
             textfield.setStyle("-fx-border-style: none");
-            textfield.setText("");}
-        if(mouseEvent.getSource() instanceof DatePicker){
+            textfield.setText("");
+        }
+        if (mouseEvent.getSource() instanceof DatePicker) {
             DatePicker textfield = (DatePicker) mouseEvent.getSource();
             textfield.setStyle("-fx-border-style: none");
-            }
         }
+    }
 
+    public void changeScene(ActionEvent event) throws IOException {
 
+        final FXMLLoader loader = new FXMLLoader();
 
+        Button button = ((Button) event.getSource());
+        Parent sceneRoot;
+        if (button.getId().equals("apply_bt")) {
+            //choose fxml Scene
+            sceneRoot = loader.load(getClass().getResourceAsStream("/fxml/welcome.fxml"));
+            root.getChildren().clear();
+            root.getChildren().add(sceneRoot);
+        } else {
+            Label label = new Label("Tut uns Leid es ist etwas schief gelaufen. Bitte starte das Programm neu");
+            sceneRoot = label;
 
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
---
+++ src/main/java/de/hdm_stuttgart_mi/Controller/roommateController.java
@@ -0,0 +1,4 @@
+package de.hdm_stuttgart_mi.Controller;
+
+public class roommateController {
+}
