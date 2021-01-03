897a649b7611e51ce8ac365407a674ba833aeccd
lea <lg067@hdm-stuttgart.de>
2021-01-03 15:47:47 +0100

    profilscreen functions

--- src/main/java/de/hdm_stuttgart_mi/Controller/NewRoommateController.java
+++ src/main/java/de/hdm_stuttgart_mi/Controller/NewRoommateController.java
@@ -45,7 +45,11 @@ public class NewRoommateController extends Supercontroller implements Initializa
     private Label birthday_lb;
     @FXML
     private GridPane root;
+    @FXML
+    private ImageView backbutton;
     Navigation nav = new Navigation();
+
+
 
     @FXML
     private void datepicked() {
@@ -54,20 +58,21 @@ public class NewRoommateController extends Supercontroller implements Initializa
         log.debug(birthday);
     }
 
+
     @FXML
     private void apply(ActionEvent e) throws IOException {
         boolean applyable = true;
         String firstname = "", lastname = "", phonenumber = "";
         if (firstname_tf.getText() == null || firstname_tf.getText().equals("") || firstname_tf.getText().matches("[^a-zA-Z]")) {
             firstname_tf.setText("Bitte trage deinen Vornamen ein. Es sind keine Zahlen erlaubt");
-            firstname_tf.setStyle("-fx-border-color: red");
+            firstname_tf.getStyleClass().add("error");
             applyable = false;
         } else {
             firstname = firstname_tf.getText();
         }
         if (lastname_tf.getText() == null || lastname_tf.getText().equals("") || lastname_tf.getText().matches("[^a-zA-Z]")) {
             lastname_tf.setText("Bitte trage deinen Nachnamen ein. Es sind keine Zahlen erlaubt");
-            lastname_tf.setStyle("-fx-border-color: red");
+            lastname_tf.getStyleClass().add("error");
             applyable = false;
         } else {
             lastname = lastname_tf.getText();
@@ -76,11 +81,11 @@ public class NewRoommateController extends Supercontroller implements Initializa
             phonenumber = phonenumber_tf.getText();
         } else {
             phonenumber_tf.setText("Nur Zahlen in der Telefonnummer erlaubt");
-            phonenumber_tf.setStyle("-fx-border-color: red");
+            phonenumber_tf.getStyleClass().add("error");
             applyable = false;
         }
         if (birthday_dp.getValue() == null) {
-            birthday_dp.setStyle("-fx-border-color: red;");
+            birthday_dp.getStyleClass().add("error");
             applyable = false;
         }
         if (applyable) {
@@ -95,7 +100,11 @@ public class NewRoommateController extends Supercontroller implements Initializa
 
 
     }
-
+    private void initIcon() throws FileNotFoundException {
+        FileInputStream backIconPath = new FileInputStream("src/main/resources/icons/pfeil.png");
+        Image backIcon= new Image(backIconPath);
+        backbutton.setImage(backIcon);
+    }
     private void initProfilpics() throws FileNotFoundException {
 
         ToggleGroup toggleGroup = new ToggleGroup();
@@ -133,32 +142,15 @@ public class NewRoommateController extends Supercontroller implements Initializa
     private void clear(MouseEvent mouseEvent) {
         if (mouseEvent.getSource() instanceof TextField) {
             TextField textfield = (TextField) mouseEvent.getSource();
-            textfield.setStyle("-fx-border-style: none");
+            textfield.getStyleClass().remove("error");
             textfield.setText("");
         }
         if (mouseEvent.getSource() instanceof DatePicker) {
             DatePicker textfield = (DatePicker) mouseEvent.getSource();
-            textfield.setStyle("-fx-border-style: none");
+            textfield.getStyleClass().remove("error");
         }
     }
 
-    public void changeScene(ActionEvent event) throws IOException {
-
-        final FXMLLoader loader = new FXMLLoader();
-
-        Button button = ((Button) event.getSource());
-        Parent sceneRoot;
-        if (button.getId().equals("apply_bt")) {
-            //choose fxml Scene
-            sceneRoot = loader.load(getClass().getResourceAsStream("/fxml/menu.fxml"));
-            root.getChildren().clear();
-            root.getChildren().add(sceneRoot);
-        } else {
-            Label label = new Label("Tut uns Leid es ist etwas schief gelaufen. Bitte starte das Programm neu");
-            sceneRoot = label;
-
-        }
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -166,8 +158,13 @@ public class NewRoommateController extends Supercontroller implements Initializa
             initProfilpics();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            log.error("File not found");
+            log.error("Profilepics not initialized");
         }
-
+        try {
+            initIcon();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            log.error("Icon not initialized");
+        }
     }
 }
--- src/main/java/de/hdm_stuttgart_mi/Controller/ProfilController.java
+++ src/main/java/de/hdm_stuttgart_mi/Controller/ProfilController.java
@@ -5,13 +5,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -23,21 +29,27 @@ import java.util.ResourceBundle;
 
 import static de.hdm_stuttgart_mi.notificationAndUsers.Navigation.currentUser;
 
-public class ProfilController extends MenuController implements Initializable {
+public class ProfilController extends Supercontroller implements Initializable {
+
+    private static final Logger log = LogManager.getLogger(ProfilController.class);
+    final private DateTimeFormatter formatter= DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    Navigation nav = new Navigation();
+
     @FXML private ImageView ownProfilePic;
-    @FXML private Button birthday_bt;
+    @FXML private ImageView editIconView;
+    @FXML private Button edit;
     @FXML private Label birthday_lb;
     @FXML private Label moveInDate;
     @FXML private Label phonenumber;
     @FXML private Label fullname_lb;
     @FXML private HBox profilpics;
     @FXML private DatePicker birthdaypicker;
+    @FXML private TextField phonenumber_tf;
+    @FXML private Button apply_bt;
 
-    final private DateTimeFormatter formatter= DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    Navigation nav = new Navigation();
 
-    public ProfilController() throws FileNotFoundException { }
 
+   // first Init Methods so everything is set up
 
     private void initPhonenumber(){
         phonenumber.setText(currentUser.getPhonenumber());
@@ -46,34 +58,13 @@ public class ProfilController extends MenuController implements Initializable {
     private void initMoveInDate(){
         moveInDate.setText("Du bist am "+currentUser.getMoveInDate().format(formatter)+" eingezogen");
     }
-
-//OnAction method for "Bearbeiten"-Button
-    @FXML
-    private void changeBirthday(){
-        //If the Button was already pressed before it changes back to "Bearbeiten" and sets the new Birthdate
-        if(birthdaypicker.isVisible()){
-
-            birthdaypicker.setVisible(false);
-            birthdaypicker.setDisable(true);
-            birthday_bt.setText("Bearbeiten");
-
-            if(birthdaypicker.getValue()!=null){
-                currentUser.setBirthday(birthdaypicker.getValue());
-                birthday_lb.setText(currentUser.getBirthday().format(formatter));
-                nav.updateCurrentUser();
-            }
-
-        }
-        //if the Button wasn't pressed before it sets the Datepicker visable and enables it
-       else {
-            birthday_bt.setText("Übernehmen");
-            birthdaypicker.setVisible(true);
-            birthdaypicker.setDisable(false);
-        }
-
+    private void initIcon() throws FileNotFoundException {
+        FileInputStream editIconPath = new FileInputStream("src/main/resources/icons/stift.png");
+        Image editIcon= new Image(editIconPath);
+        editIconView.setImage(editIcon);
+        editIconView.setFitHeight(20);
+        editIconView.setFitWidth(20);
     }
-
-
     private void setOwnPp() throws FileNotFoundException {
         FileInputStream currentPp = new FileInputStream(currentUser.getProfilepic());
         Image currentProfilePic= new Image(currentPp);
@@ -81,6 +72,70 @@ public class ProfilController extends MenuController implements Initializable {
         ownProfilePic.setFitHeight(80);
         ownProfilePic.setFitWidth(80);
     }
+
+//OnAction method for "Bearbeiten"-Button
+    @FXML
+    private void edit(){
+
+            apply_bt.setVisible(true);
+            edit.setVisible(false);
+            birthdaypicker.setVisible(true);
+            birthdaypicker.setDisable(false);
+            profilpics.setVisible(true);
+            phonenumber_tf.setVisible(true);
+    }
+//On action Method that apllys changes
+   @FXML private void apply(ActionEvent event) throws IOException {
+       boolean applyable = true;
+
+       if (birthdaypicker.getValue() != null) {
+           currentUser.setBirthday(birthdaypicker.getValue());
+           birthday_lb.setText(currentUser.getBirthday().format(formatter));
+           nav.updateCurrentUser();
+       }
+       if (!(phonenumber_tf.getText().equals(""))) {
+           if (phonenumber_tf.getText().matches("^(\\d+)$")) {
+               currentUser.setPhonenumber(phonenumber_tf.getText());
+               initPhonenumber();
+           } else {
+               phonenumber_tf.setText("Nur Zahlen in der Telefonnummer erlaubt");
+               phonenumber_tf.getStyleClass().add("error");
+               applyable = false;
+           }
+       }
+
+       if (applyable) {
+           edit.setVisible(true);
+           apply_bt.setVisible(false);
+
+           birthdaypicker.setVisible(false);
+           birthdaypicker.setDisable(true);
+           profilpics.setVisible(false);
+           phonenumber_tf.setVisible(false);
+           phonenumber_tf.setText("");
+           birthdaypicker.setValue(null);
+
+
+           BorderPane root = (BorderPane) ((Button) event.getSource()).getScene().getRoot();
+           Node sceneRoot = FXMLLoader.load(getClass().getResource("/fxml/menu.fxml"));
+           BorderPane.setMargin(sceneRoot,new Insets(0,10,0,0));
+           root.setLeft(sceneRoot);
+       }
+   }
+   //OnMouseClicked Method that clears the Textfield when clicked on it
+   @FXML private void clear(MouseEvent mouseEvent) {
+            TextField textfield = (TextField) mouseEvent.getSource();
+            textfield.getStyleClass().remove("error");
+            textfield.setText("");
+        }
+
+   //OnActionEvent Method that delets the User that is currently logged in and switches back to login screen
+   @FXML public void deleteUser(ActionEvent e) throws IOException {
+        nav.deleteFromList();
+        super.changeScene(e);
+    }
+
+
 
     private void setProfilePics() throws FileNotFoundException {
 
@@ -107,8 +162,10 @@ public class ProfilController extends MenuController implements Initializable {
                 ToggleButton button = new ToggleButton();
                 Image profilePicImage = new Image(input);
                 ImageView showProfilePic = new ImageView(profilePicImage);
-                showProfilePic.setFitWidth(100);
-                showProfilePic.setFitHeight(100);
+
+                showProfilePic.setFitWidth(80);
+                showProfilePic.setFitHeight(80);
+
                 button.setGraphic(showProfilePic);
                 button.setId("src/main/resources/images/" + profilepicPath);
 
@@ -117,6 +174,7 @@ public class ProfilController extends MenuController implements Initializable {
                         changeProfilepic(button);
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
+                        log.error("Profilpicbutton event failed");
                     }
                 });
 
@@ -126,20 +184,16 @@ public class ProfilController extends MenuController implements Initializable {
         }
     }
 
-    private void changeProfilepic(ToggleButton button) throws FileNotFoundException {
+    //OnActionEvent Method for Profilpicbuttons
 
+    private void changeProfilepic(ToggleButton button) throws FileNotFoundException {
         currentUser.setProfilepic(button.getId());
         nav.updateCurrentUser();
         setOwnPp();
         setProfilePics();
-        super.initPp();
     }
 
-    @FXML
-    public void deleteUser(ActionEvent e) throws IOException {
-        nav.deleteFromList();
-        super.changeScene(e);
-    }
+
 
 
 
@@ -154,11 +208,19 @@ public class ProfilController extends MenuController implements Initializable {
             setOwnPp();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            log.error("current ProfilPic was not initialized");
         }
         try {
             setProfilePics();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            log.error("ProfilPics to choose were not initialized");
+        }
+        try {
+            initIcon();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            log.error("Icon was not initialized");
         }
     }
 }
